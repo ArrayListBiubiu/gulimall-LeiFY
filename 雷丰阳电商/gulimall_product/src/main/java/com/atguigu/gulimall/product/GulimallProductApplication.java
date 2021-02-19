beaf4a996a218e8fe526ef249f6155cea11a9c89
biubiu <1010015306@qq.com>
2021-02-19 15:10:42 +0800

    项目

--- 雷丰阳电商/gulimall_product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ 雷丰阳电商/gulimall_product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -2,9 +2,11 @@ package com.atguigu.gulimall.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
+@EnableCaching // 开启缓存功能
 @EnableDiscoveryClient  // 开启注册中心
 @SpringBootApplication
 //@MapperScan("com.atguigu.gulimall.product.dao")
