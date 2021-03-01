f12b3e28c3156e9354dce48021abefba96848333
biubiu <1010015306@qq.com>
2021-03-01 20:11:43 +0800

    项目

--- 雷丰阳电商/gulimall_product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
+++ 雷丰阳电商/gulimall_product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
@@ -207,6 +207,7 @@ public class CategoryController {
      */
     @RequestMapping("/lockDoor")
     public String setLatch() {
+        System.out.println();
         RCountDownLatch latch = redissonClient.getCountDownLatch("CountDownLatch");
         try {
             latch.trySetCount(5); // 给一个初始值为 5
